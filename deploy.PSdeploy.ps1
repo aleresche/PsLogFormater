3bf66efd0e5cc165a6e1ca1fe319dcd919a461df
Arnaud Leresche <arnaud.leresche@b-i.com>
2019-03-08 17:22:13 +0100

    !deploy

--- deploy.PSdeploy.ps1
+++ deploy.PSdeploy.ps1
@@ -19,7 +19,7 @@ if ($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1)
 {
     Deploy Module {
         By PSGalleryModule {
-            FromSource output\$ENV:BHProjectName
+            FromSource $ENV:BHProjectName
             To PSGallery
             WithOptions @{
                 ApiKey = $ENV:NugetApiKey
